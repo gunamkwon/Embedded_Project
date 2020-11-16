43af9e7a719eef4703870b374596fa26c4f4575c
rnska96@mju.ac.kr <rnjs4487!>
2020-11-16 14:49:59 +0900

    For buzzer test
    Buzzer test

---
+++ buzzer.h
@@ -0,0 +1,11 @@
+#ifndef _BUZZER_H_
+#define _BUUZER_H_
+
+
+void doHelp(); // 도움말 출력하는 함수
+int buzzerInit(); // Buzzer 설정 On
+int buzzerPlaySound(int scale); // Buzzer 사운드 출력 함수(scale은 음(도레미...)) 
+int buzzerStopSound(); // Buzzer 사운드 그만하는 함수
+int buzzerExit(); // Buzzer 설정 Off
+
+#endif
