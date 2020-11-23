d48508497a49da091542ccb8c521fb1efa961214
rnska96@mju.ac.kr <rnjs4487!>
2020-11-23 20:01:18 +0900

    colorled.c
    ADD COLORLED

---
+++ colorled.h
@@ -0,0 +1,13 @@
+#ifndef _COLORLED_H_
+#define _COLORLED_H_
+
+int pwmLedInit();
+int pwmSetPercent();
+int pwmInactiveAll();
+
+int pwmSetGreen();
+int pwmSetRed();
+int pwmSetYellow();
+
+
+#endif
