ea16c48203dad9f0a7bbafed3a7d8fabf213c290
Kim sungjun <skdrns12@naver.com>
2020-12-11 19:55:38 +0900

    수정
    수

--- led.h
+++ led.h
@@ -3,7 +3,7 @@
 
 int ledLibInit(void);
 int ledOnOff(int ledNum,int onoff);
-int ledsOn(int ledCount,int onoff);
+int ledsOn2(int ledCount,int onoff);
 int ledLibExit(void);
 
 #define LED_DRIVER_NAME "/dev/periled"
