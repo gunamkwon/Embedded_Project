9767d4f4c5efd14ac1375c3e4de897c168edfa24
Kim sungjun <skdrns12@naver.com>
2020-12-11 19:55:25 +0900

    수정
    수

--- buzzer.h
+++ buzzer.h
@@ -4,7 +4,7 @@
 
 void doHelp(); // 도움말 출력하는 함수
 int buzzerInit(); // Buzzer 설정 On
-int buzzerPlaySound(int scale); // Buzzer 사운드 출력 함수(scale은 음(도레미...)) 
+int buzzerPlaySound2(int scale); // Buzzer 사운드 출력 함수(scale은 음(도레미...)) 
 int buzzerStopSound(); // Buzzer 사운드 그만하는 함수
 int buzzerExit(); // Buzzer 설정 Off
 
